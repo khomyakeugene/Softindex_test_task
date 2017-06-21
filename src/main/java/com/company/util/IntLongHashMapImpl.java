9ee352da53d28ed5acfddb517105139a60bdb408
khomyakeugene <khomyak_eugene@yahoo.com>
2017-06-21 14:26:02 +0300

    Softindex Test re-working

---
+++ src/main/java/com/company/util/IntLongHashMapImpl.java
@@ -0,0 +1,197 @@
+package com.company.util;
+
+/**
+ * Created by ekhomiak on 17.06.2017.
+ */
+
+public class IntLongHashMapImpl implements IntLongHashMap {
+    private static final int MAXIMUM_CAPACITY = 1 << 30;
+
+    private static final int INIT_CAPACITY_DEFAULT_VALUE = 16;
+    private static final float LOAD_FACTOR_DEFAULT_VALUE = 0.75f;
+    private static final float MULTIPLIER_DEFAULT_VALUE = 2.0f;
+    private static final long NO_ENTRY_VALUE_DEFAULT_VALUE = Long.MIN_VALUE;
+
+    private static final String ILLEGAL_INITIAL_CAPACITY_PATTERN = "Illegal initial capacity: %d";
+    private static final String ILLEGAL_LOAD_FACTOR_PATTERN = "Illegal load factor: : %f";
+    private static final String ILLEGAL_MULTIPLIER_PATTERN = "Illegal multiplier: : %f";
+
+    private int capacity;
+    private final float loadFactor;
+    private final float multiplier;
+    private final long noEntryValue;
+
+    private int[] keys;
+    private long[] values;
+    private int size;
+    private int limit;
+    private boolean entryWithNullKeyIsAssociated;
+    private long nullValue;
+
+    public IntLongHashMapImpl(int initialCapacity, float loadFactor, float multiplier, long noEntryValue) {
+        if (initialCapacity < 0 || initialCapacity > MAXIMUM_CAPACITY) {
+            throw new IllegalArgumentException(String.format(ILLEGAL_INITIAL_CAPACITY_PATTERN, initialCapacity));
+        }
+        if (Float.isNaN(loadFactor) || loadFactor <= 0 || loadFactor > 1.0) {
+            throw new IllegalArgumentException(String.format(ILLEGAL_LOAD_FACTOR_PATTERN, loadFactor));
+        }
+        if (multiplier <= 0 || Float.isNaN(multiplier)) {
+            throw new IllegalArgumentException(String.format(ILLEGAL_MULTIPLIER_PATTERN, multiplier));
+        }
+
+        this.loadFactor = loadFactor;
+        this.multiplier = multiplier;
+        this.noEntryValue = noEntryValue;
+        nullValue = noEntryValue;
+
+        keys = new int[initialCapacity];
+        values = new long[initialCapacity];
+
+        setCapacity(initialCapacity);
+    }
+
+    public IntLongHashMapImpl(int initialCapacity, float loadFactor, float multiplier) {
+        this(initialCapacity, loadFactor, multiplier, NO_ENTRY_VALUE_DEFAULT_VALUE);
+    }
+
+    public IntLongHashMapImpl(int initialCapacity, float loadFactor) {
+        this(initialCapacity, loadFactor, MULTIPLIER_DEFAULT_VALUE);
+    }
+
+    public IntLongHashMapImpl(int initialCapacity) {
+        this(initialCapacity, LOAD_FACTOR_DEFAULT_VALUE);
+    }
+
+    public IntLongHashMapImpl() {
+        this(INIT_CAPACITY_DEFAULT_VALUE);
+    }
+
+    public long getNoEntryValue() {
+        return noEntryValue;
+    }
+
+    private void setCapacity(int capacity) {
+        this.capacity = capacity;
+        this.limit = (int) (capacity * loadFactor);
+
+        // <limit> should be LESS than "card-size", because method <getIndex>, on purpose not to use
+        // "full keys circle", expects that there always should be at least one "empty"
+        // (with 0-value) entry in keys array
+        if (limit >= capacity) {              // ">=" is actually excess right here, but let's keep it
+            limit = capacity - 1;
+        }
+    }
+
+    // Hash code is using to determine index in hash table, so hash code should be positive
+    private static int hashCode(int value) {
+        return Integer.hashCode(value) & Integer.MAX_VALUE;
+    }
+
+    private int indexFor(int key) {
+        return hashCode(key) % capacity;
+    }
+
+    private void rawPut(int key, long value) {
+        int index = indexFor(key);
+        while (keys[index] != 0) {
+            if ((++index) >= capacity) {
+                index = 0;
+            }
+        }
+
+        keys[index] = key;
+        values[index] = value;
+        size++;
+    }
+
+    private void reAllocTables() {
+        setCapacity((int) (capacity * multiplier));
+
+        int[] oldKeys = keys;
+        long[] oldValues = values;
+
+        keys = new int[capacity];
+        values = new long[capacity];
+
+        size = 0;
+        for (int i = 0; i < oldKeys.length; i++) {
+            if (oldKeys[i] != 0) {
+                rawPut(oldKeys[i], oldValues[i]);
+            }
+        }
+    }
+
+    private int getIndex(int key) {
+        int result = -1;
+
+        int index = indexFor(key);
+        // With strong expectation that there always should be at least one "empty" (with 0-value) entry in keys array
+        while (keys[index] != 0 && keys[index] != key) {
+            index++;
+            if (index >= capacity) {
+                index = 0;
+            }
+        }
+        if (keys[index] == key) {
+            result = index;
+        }
+
+        return result;
+    }
+
+    @Override
+    public long put(int key, long value) {
+        // There is deliberately no control that <value> can be equal to <noEntryValue>. The "side-effect" of such
+        // approach is that <noEntryValue> can be returned in case if some entry(ies) really contain(s) <noEntryValue>
+        // and also in case, if there no entry associated with given key
+        long result = noEntryValue;
+
+        // Separately check for "null key" (0 is default ("empty") initial value for array data, so "null key" 0
+        // is deliberately not "described" as a constant here
+        if (key == 0) {
+            result = nullValue;
+            nullValue = value;
+            entryWithNullKeyIsAssociated = true;
+        } else {
+            // It is obligatory to do <reAllocTables> if the "card" is full BEFORE call of <getIndex> because
+            // <getIndex>, on purpose not to use "full keys circle", expects that there should be at least one "empty"
+            // (with 0-value) entry in keys array
+            if (size >= limit) {
+                reAllocTables();
+            }
+
+            int index = getIndex(key);
+            if (index >= 0) {
+                result = values[index];
+                values[index] = value;
+            } else {
+                rawPut(key, value);
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public long get(int key) {
+        long result = noEntryValue;
+
+        // Separately check for "null key" (0 is default ("empty") value for array data, so "null key" 0 is deliberately
+        // not "described" as a constant here
+        if (key == 0) {
+            result = nullValue;
+        } else {
+            int index = getIndex(key);
+            if (index >= 0) {
+                result = values[index];
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public int size() {
+        return entryWithNullKeyIsAssociated ? size + 1 : size;
+    }
+}
--- src/test/java/com/company/util/IntLongHashMapTest.java
+++ src/test/java/com/company/util/IntLongHashMapTest.java
@@ -12,18 +12,26 @@ import static org.junit.Assert.assertEquals;
  * Created by ekhomiak on 16.06.2017.
  */
 public class IntLongHashMapTest {
-    private static final int RANDOM_INT_UPPER_BOUND = 1000; // To have the possibility to regulate "probability" of repetitive keys
-    private static final int TEST_QUANTITY = 10000;
+    private static final int RANDOM_INT_UPPER_BOUND = 10000; // To have the ability to regulate "probability" of repetitive keys
+    private static final int TEST_QUANTITY = 100000;
 
     private static final String SIZE_METHOD_SIGNATURE = "size()";
     private static final String PUT_METHOD_PATTERN = "put(%d, %d)";
     private static final String GET_METHOD_PATTERN = "get(%d)";
     private static final String METHOD_FAILED_PATTERN = "The method %s.%s failed:";
     private static final String TESTING_ENTRY_PATTERN = "The testing entry N %d is: key: %d, value: %d";
+    private static final String EMPTY_CARD_TEST_IS_OK = "Empty card test is ok!";
+    private static final String NULL_KEY_TEST_IS_OK = "Null key test is ok!";
+    private static final String NO_ENTRY_VALUE_TEST_IS_OK = "No entry value test is ok!";
+    private static final String THRESHOLD_KEYS_TEST_IS_OK = "Threshold keys test is ok!";
 
-    private Map<Integer, Long> expectedValueSupplier = new HashMap<>();
-    private IntLongHashMap intLongHashMap = new IntLongHashMap();
-    private long noEntryValue = intLongHashMap.getNoEntryValue();
+    private Map<Integer, Long> expectedValueSupplier;
+    private IntLongHashMap intLongHashMap;
+    private long noEntryValue;
+
+    // To have the ability to use predetermined "etalon" keys
+//    private int[] predeterminedKeys = {350, -746, -147, -346, 92, -902, -274, -530, 127, -20, -122, -543, 85, -981, -409, -374, 32};
+    private int[] preDeterminedKeys = {};
 
     private int getRandomInteger() {
         return RandomDataGenerator.getRandomInteger(RANDOM_INT_UPPER_BOUND);
@@ -37,6 +45,14 @@ public class IntLongHashMapTest {
         return String.format(METHOD_FAILED_PATTERN, intLongHashMap.getClass().getName(), methodName);
     }
 
+    private String getIncorrectGetMethodResultMessage(int key) {
+        return getIncorrectMethodResultMessage(String.format(GET_METHOD_PATTERN, key));
+    }
+
+    private String getIncorrectPutMethodResultMessage(int key, long value) {
+        return getIncorrectMethodResultMessage(String.format(PUT_METHOD_PATTERN, key, value));
+    }
+
     private void sizeTest() {
         assertEquals(getIncorrectMethodResultMessage(SIZE_METHOD_SIGNATURE),
                 expectedValueSupplier.size(), intLongHashMap.size());
@@ -45,24 +61,59 @@ public class IntLongHashMapTest {
     private void getTest(int key) {
         Long expectedValue = expectedValueSupplier.get(key);
 
-        assertEquals(getIncorrectMethodResultMessage(String.format(GET_METHOD_PATTERN, key)), (expectedValue == null) ?
+        assertEquals(getIncorrectGetMethodResultMessage(key), (expectedValue == null) ?
                 noEntryValue : expectedValue, intLongHashMap.get(key));
     }
 
     private void putTest(int key, long value) {
         Long expectedPreviousValue = expectedValueSupplier.put(key, value);
 
-        assertEquals(getIncorrectMethodResultMessage(String.format(PUT_METHOD_PATTERN, key, value)),
+        assertEquals(getIncorrectPutMethodResultMessage(key, value),
                 (expectedPreviousValue == null) ? noEntryValue : expectedPreviousValue, intLongHashMap.put(key, value));
     }
 
-    @Test(timeout = 5000)
-    public void sizePutGetTest() throws Exception {
+    private void emptyCardTest() throws Exception {
+        assertEquals(getIncorrectMethodResultMessage(SIZE_METHOD_SIGNATURE),0, intLongHashMap.size());
+
+        int key = getRandomInteger();
+        assertEquals(getIncorrectGetMethodResultMessage(key),noEntryValue, intLongHashMap.get(key));
+
+        System.out.println(EMPTY_CARD_TEST_IS_OK);
+    }
+
+    private void nullKeyTest() throws Exception {
+        int nullKey = 0;
+        long value = getRandomLong();
+
+        expectedValueSupplier.put(nullKey, value); // Important to synchronize expectedValueSupplier.size() and intLongHashMap.size()
+        intLongHashMap.put(nullKey, value);
+        assertEquals(getIncorrectGetMethodResultMessage(nullKey),value, intLongHashMap.get(nullKey));
+
+        System.out.println(NULL_KEY_TEST_IS_OK);
+    }
+
+    private void thresholdKeysTest() {
+        int thresholdKey = Integer.MIN_VALUE;
+        long value = getRandomLong();
+        expectedValueSupplier.put(thresholdKey, value); // Important to synchronize expectedValueSupplier.size() and intLongHashMap.size()
+        intLongHashMap.put(thresholdKey, value);
+        assertEquals(getIncorrectGetMethodResultMessage(thresholdKey),value, intLongHashMap.get(thresholdKey));
+
+        thresholdKey = Integer.MAX_VALUE;
+        value = getRandomLong();
+        expectedValueSupplier.put(thresholdKey, value); // Important to synchronize expectedValueSupplier.size() and intLongHashMap.size()
+        intLongHashMap.put(thresholdKey, value);
+        assertEquals(getIncorrectGetMethodResultMessage(thresholdKey),value, intLongHashMap.get(thresholdKey));
+
+        System.out.println(THRESHOLD_KEYS_TEST_IS_OK);
+    }
+
+    private void allMethodTest() throws Exception {
         int key;
         long value;
 
         for (int index = 0; index < TEST_QUANTITY; index++) {
-            key = getRandomInteger();
+            key = (index < preDeterminedKeys.length) ? preDeterminedKeys[index] : getRandomInteger();
             value = getRandomLong();
 
             System.out.println(String.format(TESTING_ENTRY_PATTERN, index, key, value));
@@ -78,5 +129,49 @@ public class IntLongHashMapTest {
         for (int i = 0; i < TEST_QUANTITY; i++) {
             getTest(getRandomInteger());
         }
+    }
+
+    private void sizePutGetNoEntryValueTest() throws Exception {
+        int key = getRandomInteger();
+        long value = noEntryValue;
+
+        getTest(key);
+        putTest(key, value);
+        sizeTest();
+        getTest(key);
+
+        System.out.println(NO_ENTRY_VALUE_TEST_IS_OK);
+    }
+
+    @Test(timeout = 5000)
+    public void sizePutGetTest() throws Exception {
+        // All the tests - in one "main" test-method in order to simplify test order achievement
+        expectedValueSupplier = new HashMap<>();
+        intLongHashMap = new IntLongHashMapImpl();
+        noEntryValue = intLongHashMap.getNoEntryValue();
+
+        // Empty card test
+        emptyCardTest();
+
+        // No entry value test
+        sizePutGetNoEntryValueTest();
+
+        // Null key test
+        nullKeyTest();
+
+        // Threshold keys test
+        thresholdKeysTest();
+
+        // The whole functionality tests
+        allMethodTest();
+
+        // No entry value test again
+        sizePutGetNoEntryValueTest();
+
+        // Null key test again
+        nullKeyTest();
+
+        // Threshold keys test again
+        thresholdKeysTest();
     }
 }
